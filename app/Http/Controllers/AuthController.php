0b7ea7cbd94015d4de07a1fb8942413b88efe470
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-12-09 22:41:10 +0900

    アップデート

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
