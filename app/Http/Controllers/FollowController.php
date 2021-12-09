3a2509657d9540c8da0a31c09eb86d0e94fba527
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-12-09 21:19:20 +0900

    アップデート

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Follow;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class FollowController extends Controller
 {
